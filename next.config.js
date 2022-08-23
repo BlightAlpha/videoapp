7a99adac68aecd64879719281265ed31ff51f227
BlightAlpha <86532314+BlightAlpha@users.noreply.github.com>
2022-08-23 10:48:53 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   typescript: {
     ignorBuilderErrors: true,
-  }
+  },
   reactStrictMode: true,
   images: {
   domains: ['brm.io','lh3.googleusercontent.com','media-exp1.licdn.com'
