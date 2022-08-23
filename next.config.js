f5704231c044fffe501301e76c441a32f33a1096
BlightAlpha <Surapa.phrompha@gmail.com>
2022-08-22 23:21:24 -0500

    First Commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+  domains: ['brm.io','lh3.googleusercontent.com','media-exp1.licdn.com'
+],
+  },
+};
+
+module.exports = nextConfig;
---
+++ utils/client.ts
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: 'qbux0j27',
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: false,
+  token: 'skbH2vqoxDLciAoXeqtTQSYGUONsQp8P3UmDyTd9OlScpIUuAx9WKF3Xbqri0HcP7CA9p5fjOw9J9Ebs2VwSYIfvuhWpRYl3zgvfVvjEXI54qdRISwNt6UN2A3cUebHLH9dKdo0B2zJjQ3vbTAJ470etyWrb4cKRp2qX3nh3w8TluGZPCPkE',
+});
